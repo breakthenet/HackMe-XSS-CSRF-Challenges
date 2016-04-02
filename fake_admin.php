75e8057ca179bf66aa33e4102aef780588dc0183
emeth- <seanybob@gmail.com>
2016-04-01 21:39:52 -0500

    add phantomjs

---
+++ fake_admin.php
@@ -0,0 +1,26 @@
+<?php
+
+    use JonnyW\PhantomJs\Client;
+
+    $client = Client::getInstance();
+
+    $client->getEngine()->setPath('/app/vendor/phantomjs/bin');
+    
+    /** 
+     * @see JonnyW\PhantomJs\Message\Request 
+     **/
+    $request = $client->getMessageFactory()->createRequest('http://jonnyw.me', 'GET');
+
+    /** 
+     * @see JonnyW\PhantomJs\Message\Response 
+     **/
+    $response = $client->getMessageFactory()->createResponse();
+
+    // Send the request
+    $client->send($request, $response);
+
+    if($response->getStatus() === 200) {
+
+        // Dump the requested page content
+        echo $response->getContent();
+    }
