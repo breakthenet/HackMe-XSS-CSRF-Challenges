5cd554935f0ade4c076659269abf31eeb1955a49
emeth- <seanybob@gmail.com>
2016-04-07 19:19:09 -0500

    debug

--- fake_admin_browser.js
+++ fake_admin_browser.js
@@ -37,11 +37,13 @@ page.open(base_url+'authenticate.php', 'post', 'username=admin&password=cupcake&
                 } else {
                     console.log("Successfully opened "+base_url+msg);
                     page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
-                        console.log("Extracting any links in forum signature...");
+                        console.log("Extracting any links in profile signature...");
                         page.evaluate(function() {
                             $("#profile_sig").find('a').each(function() {
+                                console.log("egg");
                                 console.log(jQuery(this).attr('href'));
                             });
+                            console.log("fish");
                         });
                     });
                 }
